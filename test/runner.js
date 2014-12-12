d19824e5ba24a54cca6f15abe158b12375c428a9
Stuart Watt <stuart@morungos.com>
2014-12-12 11:08:42 -0500

    Allow an existing connection to be passed in, as well as an object connection. Handy for using the same pool

--- test/runner.js
+++ test/runner.js
@@ -8,8 +8,6 @@ var Knex = require('knex');
 
 describe('log4js-knex', function () {
 
-    var knex = undefined;
-
     var log4jsConfig = {table: 'log', knex: {client: 'sqlite', connection: {filename: './test.db'}}}
 
     beforeEach(function (done) {
@@ -23,13 +21,26 @@ describe('log4js-knex', function () {
     });
 
 
-    it('should throw an Error when the connectionString is not set', function () {
+    it('should throw an Error when there\'s no configuration set', function () {
 
         (function () { return log4js.addAppender(log4jsKnex.appender()); }).should.throw();
     });
 
-    it('should log to the mongo database when initialized through the configure function', function (done) {
+    it('should log to the database when initialized through the configure function', function (done) {
         log4js.addAppender(log4jsKnex.configure(log4jsConfig));
+        log4js.getLogger().info('Ready to log!');
+
+        // Can't immediately finish the test, as database writing is asynchronous. Wait a short while for
+        // promises to resolve. 
+        setTimeout(function () {
+            done();
+        }, 100);
+    });
+
+    it('should log to the database when initialized with an existing connection', function (done) {
+        var knex = Knex({client: 'sqlite', connection: {filename: './test.db'}});
+        var config = {table: 'log', knex: knex}
+        log4js.addAppender(log4jsKnex.configure(config));
         log4js.getLogger().info('Ready to log!');
 
         // Can't immediately finish the test, as database writing is asynchronous. Wait a short while for
