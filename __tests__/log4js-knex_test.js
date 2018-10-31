2e333c15371c94fbc47982bab256213a7f24cf26
Stuart Watt <stuart@morungos.com>
2018-10-30 20:46:36 -0400

    Updated to newer knex.js and tested more

--- __tests__/log4js-knex_test.js
+++ __tests__/log4js-knex_test.js
@@ -17,11 +17,14 @@ describe('log4js-knex', () => {
     expect(() => log4js.addAppender(log4jsKnex.configure({}))).toThrow(/connection parameters are missing/);
   });
 
-  it('should initialize knex correctly', () => {
-    const handler = {
-      insert: jest.fn(() => Promise.resolve())
-    }
+  it('should initialize knex correctly when insert succeeds', () => {
+    const insert = jest.fn();
+    insert.mockImplementationOnce(() => Promise.resolve());
+    const handler = {insert: insert};
     const connection = jest.fn(() => handler);
+    connection.schema = {
+      createTable: jest.fn(() => Promise.resolve())
+    };
     require('knex').__setMockConnection(connection);
     const appender = log4jsKnex.configure({
       knex: {
@@ -31,7 +34,36 @@ describe('log4js-knex', () => {
         }
       }
     }, layouts);
-    appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}});
+    return appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}})
+      .then(() => {
+        expect(insert).toBeCalledTimes(1);
+        expect(connection.schema.createTable).not.toBeCalled();
+      });
+  });
+
+  it('should initialize knex correctly when insert fails initially', () => {
+    const insert = jest.fn();
+    insert.mockImplementationOnce(() => Promise.reject(new Error("Missing table")));
+    insert.mockImplementationOnce(() => Promise.resolve());
+    const handler = {insert: insert};
+    const connection = jest.fn(() => handler);
+    connection.schema = {
+      createTable: jest.fn(() => Promise.resolve())
+    };
+    require('knex').__setMockConnection(connection);
+    const appender = log4jsKnex.configure({
+      knex: {
+        client: 'mysql',
+        connection: {
+          host: 'example.com'
+        }
+      }
+    }, layouts);
+    return appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}})
+      .then(() => {
+        expect(insert).toBeCalledTimes(2);
+        expect(connection.schema.createTable).toBeCalled();
+      })
   });
 
 });
