2878e079ffa25eb096fe06d0cbc5b1e0405b05fc
Stuart Watt <stuart@morungos.com>
2018-10-31 11:01:19 -0400

    Removed tests for table creation
    See #6

--- __tests__/log4js-knex_test.js
+++ __tests__/log4js-knex_test.js
@@ -43,45 +43,6 @@ describe('log4js-knex', () => {
       });
   });
 
-  it('should initialize knex correctly when insert fails initially', () => {
-    const insert = jest.fn();
-    insert.mockImplementationOnce(() => Promise.reject(new Error("Missing table")));
-    insert.mockImplementationOnce(() => Promise.resolve());
-    const handler = {insert: insert};
-    const connection = jest.fn(() => handler);
-    connection.schema = {
-      createTable: jest.fn(() => Promise.resolve())
-    };
-    require('knex').__setMockConnection(connection);
-    const appender = log4jsKnex.configure(knexConfig, layouts);
-    return appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}})
-      .then(() => {
-        expect(insert).toBeCalledTimes(2);
-        expect(connection.schema.createTable).toBeCalled();
-      })
-  });
-
-  it('should fail when table creation fails', () => {
-    const insert = jest.fn();
-    insert.mockImplementationOnce(() => Promise.reject(new Error("Missing table")));
-    insert.mockImplementationOnce(() => Promise.resolve());
-    const handler = {insert: insert};
-    const connection = jest.fn(() => handler);
-    connection.schema = {
-      createTable: jest.fn(() => Promise.reject(new Error("Can't create table")))
-    };
-    require('knex').__setMockConnection(connection);
-    const appender = log4jsKnex.configure(knexConfig, layouts);
-    
-    const result = appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}})
-      .then(() => {
-        expect(insert).toBeCalledTimes(2);
-        expect(connection.schema.createTable).toBeCalled();
-      });
-
-    return expect(result).rejects.toThrow(/Missing table/);
-  });
-
   it('should handle a configured layout', () => {
     const insert = jest.fn();
     insert.mockImplementationOnce(() => Promise.resolve());
@@ -106,49 +67,4 @@ describe('log4js-knex', () => {
       });
   });
 
-  it('should throw the original error if needed', () => {
-    const insert = jest.fn();
-    insert.mockImplementationOnce(() => Promise.reject(new Error("Unexpected error")));
-    insert.mockImplementationOnce(() => Promise.reject(new Error("Something else")));
-    const handler = {insert: insert};
-    const connection = jest.fn(() => handler);
-    connection.schema = {
-      createTable: jest.fn(() => Promise.resolve())
-    };
-    require('knex').__setMockConnection(connection);
-    const appender = log4jsKnex.configure(knexConfig, layouts);
-    const result = appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}});
-
-    return expect(result).rejects.toThrow(/Unexpected error/);
-  });
-
-  it('should handle the table creation', () => {
-    const insert = jest.fn();
-    insert.mockImplementationOnce(() => Promise.reject(new Error("Missing table")));
-    insert.mockImplementationOnce(() => Promise.resolve());
-    const handler = {insert: insert};
-    const connection = jest.fn(() => handler);
-    connection.schema = {
-      createTable: jest.fn(() => Promise.resolve())
-    };
-    require('knex').__setMockConnection(connection);
-    const appender = log4jsKnex.configure(knexConfig, layouts);
-    return appender({data: [], level: {level: "HIGH"}, logger: {category: "default"}})
-      .then(() => {
-        expect(insert).toBeCalledTimes(2);
-        expect(connection.schema.createTable).toBeCalledWith('log', expect.any(Function));
-        
-        const callback = connection.schema.createTable.mock.calls[0][1];
-        expect(callback).toBeInstanceOf(Function);
-
-        const table = {};
-        table.increments = jest.fn();
-        table.timestamp = jest.fn(() => table);
-        table.string = jest.fn(() => table);
-        table.integer = jest.fn(() => table);
-        table.notNullable = jest.fn(() => table);
-
-        return callback(table);
-      })
-  });
 });
